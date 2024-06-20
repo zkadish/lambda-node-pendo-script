43f75abe0d88b3dc859735964dbb56609a93aad2
zach kadish <zachkadish@gmail.com>
2024-06-20 00:12:07 -0700

    init commit

---
+++ quotes-scraper/app.js
@@ -0,0 +1,60 @@
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+const crypto = require('crypto');
+
+// const url = "http://quotes.toscrape.com/";
+// https://lxgplhwh03.execute-api.us-west-1.amazonaws.com/Prod/quotes
+
+// const url = "https://lxgplhwh03.execute-api.us-west-1.amazonaws.com/Prod/quotes";
+
+exports.lambdaHandler = async (_event, _context) => {
+  const appKey = _event.queryStringParameters.key;
+  const url = `https://cdn.pendo.io/agent/static/${appKey}/pendo.js`;
+  try {
+    // const response = await axios(url);
+    const types = [
+      // 'sha256',
+      // 'sha384',
+      'sha512',
+    ];
+
+    async function getResourceContent(url) {
+      const response = await fetch(url);
+      const text = await response.text();
+      return text;
+    }
+
+    // const pendoJs = await getResourceContent(url);
+    function getHash(type, content) {
+      return crypto.createHash(type).update(content, 'utf8').digest('base64');
+    }
+    
+    async function getBase64HashFromUrl(types, url) {
+      const content = await getResourceContent(url);
+      return types.map(type => `${type}-${getHash(type, content)}`).join(' ');
+    }
+
+    const hash = await getBase64HashFromUrl(types, url);
+
+    function getResourceHTML(url, hash) {
+      return `<script src="${url}" integrity="${hash}" crossorigin="anonymous"></script>`;
+    }
+
+    const htmlScript = await getResourceHTML(url, hash);
+
+
+
+    return {
+      statusCode: 200,
+      body: htmlScript,
+      // body: JSON.stringify({response: htmlScript}),
+      // body: pendoJs.toString(),
+      // body: JSON.stringify(appKey),
+      // body: JSON.stringify(_event),
+      // body: JSON.stringify(_context),
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
